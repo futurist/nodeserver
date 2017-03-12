88e144c9c976ec964e82fe3a8ff5519f06a51366
1111hui <26634873@qq.com>
2017-03-12 19:53:27 +0800

    add echoCallback func

--- app.js
+++ app.js
@@ -1,163 +1,176 @@
 'use strict'
 
 var http = require('http'),
-    httpProxy = require('http-proxy');
-var util = require('util');
-var url = require('url');
-var path = require('path');
-var fs = require('fs');
-var mime = require('mime');
-var staticServer = require('node-static');
+    httpProxy = require('http-proxy')
+var util = require('util')
+var url = require('url')
+var path = require('path')
+var fs = require('fs')
+var mime = require('mime')
+var staticServer = require('node-static')
 
 var rootDir = '/home/1111hui/public_html'
 
-var fileServer = new staticServer.Server(rootDir);
-var proxy = new httpProxy.createProxyServer({xfwd:true, ws:true, changeOrigin:true});
+var fileServer = new staticServer.Server(rootDir)
+var proxy = new httpProxy.createProxyServer({xfwd: true, ws: true, changeOrigin: true})
 
 var apacheUrls = [
-  /\.php|\.py$/,
-];
+    /\.php|\.py$/
+]
 
 var defaultIndex = [
   'index.html',
   'index.php',
-  'index.py',
-];
-
+  'index.py'
+]
 
 function* isExists (files, findCB, resultCB) {
-	var filesObj = files.map(v=> ({file:v, isFile:null}) )
-	for(let v of filesObj) {
-		let foundFile = yield fs.stat(v.file, (err, stat)=>{ findCB(err,stat,v,filesObj) } )
-		if(foundFile) return resultCB&&resultCB(foundFile)
-	}
-	resultCB(null)
+  var filesObj = files.map(v => ({file: v, isFile: null}))
+  for (let v of filesObj) {
+    let foundFile = yield fs.stat(v.file, (err, stat) => { findCB(err, stat, v, filesObj) })
+    if (foundFile) return resultCB && resultCB(foundFile)
+  }
+  resultCB(null)
 }
 
-function fileExists(filePath) {
+function fileExists (filePath) {
   try {
     return fs.statSync(filePath).isFile()
-  } catch(err) {
-    return !(err && err.code === 'ENOENT');
+  } catch (err) {
+    return !(err && err.code === 'ENOENT')
   }
 }
 
-function parseUrl(uri){
-  var urlObj = url.parse(uri);
+function parseUrl (uri) {
+  var urlObj = url.parse(uri)
   var pathname = urlObj.pathname
   urlObj.dirname = path.dirname(pathname)
   urlObj.filename = path.basename(pathname)
   return urlObj
 }
 
-function doApache (request, response){
-      // serve .php using apache
-      proxy.web(request, response, {
-        target: 'http://127.0.0.1:8181'
-      });
+function doApache (request, response) {
+  // serve .php using apache
+  proxy.web(request, response, {
+    target: 'http://127.0.0.1:8181'
+  })
 }
 
-function doJsonAPI (request, response){
-      // serve .php using apache
-      proxy.web(request, response, {
-        target: 'http://127.0.0.1:4000'
-      });
+function doJsonAPI (request, response) {
+  // serve .php using apache
+  proxy.web(request, response, {
+    target: 'http://127.0.0.1:4000'
+  })
 }
 
-function doNodeInspector (request, response){
-      // serve node-inspector
-      proxy.web(request, response, {
-        target: 'http://127.0.0.1:8080/?ws=127.0.0.1:8080&port=5858'
-      });
+function echoCallback (req, res) {
+  var str = '\n'
+  str += req.method + ' '
+  str += req.url + '\n'
+  str += JSON.stringify(req.headers) + '\n'
+  var body = ''
+  req.on('data', function (chunk) {
+    body += chunk
+  })
+  req.on('end', function () {
+    fs.appendFile(
+      'message.txt',
+      str + body,
+      'utf8',
+      e => {
+        console.log(e)
+        res.end('ok')
+      }
+    )
+  })
 }
 
+function doNodeInspector (request, response) {
+  // serve node-inspector
+  proxy.web(request, response, {
+    target: 'http://127.0.0.1:8080/?ws=127.0.0.1:8080&port=5858'
+  })
+}
 
-
-
-function doStatic (request, response){
+function doStatic (request, response) {
   // serve static file using node-static
-   request.addListener('end', function () {
-   	  // add mime type to header
-	  var pathname = parseUrl(request.url).pathname;
-   	  response.setHeader('content-type', mime.lookup( pathname ) );
-   	  
-      fileServer.serve(request, response, function (err, result) {
-          if (err) { // There was an error serving the file
-              console.error("Error serving " + request.url + " - " + err.message);
-              // Respond to the client
-              response.writeHead(err.status, err.headers);
-              response.end();
-          }
-      });
+  request.addListener('end', function () {
+   	// add mime type to header
+	  var pathname = parseUrl(request.url).pathname
+   	response.setHeader('content-type', mime.lookup(pathname))
+
+    fileServer.serve(request, response, function (err, result) {
+      if (err) { // There was an error serving the file
+        console.error('Error serving ' + request.url + ' - ' + err.message)
+        // Respond to the client
+        response.writeHead(err.status, err.headers)
+        response.end()
+      }
+    })
   }).resume()
 }
 
-function route(pathname, request, response){
+function route (pathname, request, response) {
+  var isApache = apacheUrls.some(function (regex) {
+    return regex.test(pathname)
+  })
+  if (isApache) return doApache(request, response)
 
-    var isApache = apacheUrls.some(function (regex) {
-      return regex.test( pathname );
-    });
-    if(isApache) return doApache(request, response);
-
-    return doStatic(request, response);
+  return doStatic(request, response)
 }
 
 http.createServer(function (request, response) {
-  var pathname = parseUrl(request.url).pathname;
-  var filename = rootDir+pathname;
-  var notFound = ()=>{
-  		response.statusCode = 404;
-		response.statusMessage = 'Not found';
-		return response.end();
+  var pathname = parseUrl(request.url).pathname
+  var filename = rootDir + pathname
+  var notFound = () => {
+  	response.statusCode = 404
+    response.statusMessage = 'Not found'
+    return response.end()
   }
 
-  fs.stat( filename, function(err, stat){
-
-    if(err || !stat ){
-
+  fs.stat(filename, function (err, stat) {
+    if (err || !stat) {
     	// check is virtul dir/file
-	    var isJsonAPI = /^\/json-api\//.test( pathname );
-	    if(isJsonAPI) return doJsonAPI(request, response);
+	    var isJsonAPI = /^\/json-api\//.test(pathname)
+	    if (isJsonAPI) return doJsonAPI(request, response)
 
-		var isNodeInspector = /^\/node-debug\//.test(pathname);
-		if(isNodeInspector) return doNodeInspector(request,response);
+      var isNodeInspector = /^\/node-debug\//.test(pathname)
+      if (isNodeInspector) return doNodeInspector(request, response)
 
-		// not found
-		return notFound()
+      // console.log(pathname)
+      if (/^\/echo-callback/.test(pathname)) return echoCallback(request, response)
+
+		  // not found
+      return notFound()
     }
 
-    if( stat.isDirectory() ){
+    if (stat.isDirectory()) {
       // console.log('dir', pathname)
 
-      	// using generator function + async to not block when find a index file
-		var it = isExists(
-			defaultIndex.map( v=>path.join(filename, v) ), // index files path array
-			function findFileFunc(err, stat, f, filesObj) {
-				f.isFile = Boolean(stat&&stat.isFile());
-				for(let v of filesObj) {
-					if(typeof v.isFile!=='boolean') break;
-					if(v.isFile) {
-						it.next(v.file)  // here terminate the generator, with { value: undefined, done: true }
-						return
-					}
-				}
-				it.next();
-			},
-			function resultCB(indexFile) {
-				if(indexFile) route(indexFile, request, response)
-				else notFound()
-			}
-		)
-		it.next()
-
+      // using generator function + async to not block when find a index file
+      var it = isExists(
+			  defaultIndex.map(v => path.join(filename, v)), // index files path array
+			  function findFileFunc (err, stat, f, filesObj) {
+          f.isFile = Boolean(stat && stat.isFile())
+          for (let v of filesObj) {
+            if (typeof v.isFile !== 'boolean') break
+            if (v.isFile) {
+              it.next(v.file)  // here terminate the generator, with { value: undefined, done: true }
+              return
+            }
+          }
+          it.next()
+        },
+			  function resultCB (indexFile) {
+          if (indexFile) route(indexFile, request, response)
+          else notFound()
+        }
+		  )
+      it.next()
     } else {
       // console.log('file', pathname)
       route(pathname, request, response)
     }
-
-
-
   })
-
-}).listen(80);
+}).listen(80)
 
